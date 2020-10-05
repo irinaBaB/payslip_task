14effe4e91d8762eeafabdc33fc6c109f2d96070
Irina Babicheva <irina.babicheva@myob.com>
2020-10-05 20:08:45 +1100

    user details enter

---
+++ payslip_kata.py
@@ -0,0 +1,47 @@
+print ('Welcome to the payslip generator')
+
+# Input details: (
+# name,
+# surname,
+# annual salary,
+# super
+# rate,
+# payment start date,
+# payment end date)
+
+
+#Calculate:
+# pay period (based on entered date  01 March - 31 March (year?))
+# gross income
+# income tax
+# net income
+# super
+
+# assumption (no prorata)
+# if I enter 31 of March - as start date = 1-31 of March
+
+
+
+
+def user_details():
+    user_name = False
+    surname = False
+    while user_name == False and surname == False:
+        user_name = input("Please enter your name: ")
+        surname = input("PLease enter the surname: ")
+        if len(user_name) == 0:
+            print("Sorry, the name is incorrect, please enter the name")
+        elif len(surname) == 0:
+            print(
+                "Sorry, the surname is incorrect, please enter the correct surname")
+        print(user_name.title(), surname.title())
+        return (user_name.title(), surname.title())
+
+
+
+user_details()
+
+#assuming we only firmly calculating monthly payslip, only based on start date
+def pay_period():
+    start_date=''
+    end_date=''
