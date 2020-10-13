e5d04728dfe1a7fa57dc132b76c073041539db31
Irina Babicheva <irina.babicheva@myob.com>
2020-10-14 09:08:09 +1100

    adding the blocks

---
+++ kata_2/abc.py
@@ -0,0 +1,11 @@
+
+# 1. get collection of the blocks
+# 2. be able to prinnt each row (item) from collection
+
+
+blocks =['(B O)','(X K)','(D Q)','(C P)','(N A)','(G T)','(R E)','(T G)','(Q D)','(F S)','(J W)','(H U)','(V I)',
+         '(A N)','(O B)','(E R)','(F S)','(L Y)','(P C)','(Z M)']
+
+for item in blocks:
+    print(item)
+    
