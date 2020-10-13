8963510d537bb84652baa9cb3338576b753216cb
Irina Babicheva <irina.babicheva@myob.com>
2020-10-14 10:01:39 +1100

    steps what need to be done

--- kata_2/abc.py
+++ kata_2/abc.py
@@ -8,4 +8,11 @@ blocks =['(B O)','(X K)','(D Q)','(C P)','(N A)','(G T)','(R E)','(T G)','(Q D)'
 
 for item in blocks:
     print(item)
-    
+
+# list of blocks to begin
+# Create new empty list to set used value there
+# create a loop - when user enter the Letter
+#  -Capitilise it (any letter user entered)
+#  -Check if that letter is already used in the used_list (give an error message and end) print 'False'
+#  -Check if any letter is available in any blocks that available and print 'True'
+# will think about some tests (not the focus  right now)
