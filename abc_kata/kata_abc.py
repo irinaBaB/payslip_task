e3183182def37766217cbaeb1c74b1cb8aaf9aee
Irina Babicheva <irina.babicheva@myob.com>
2020-10-14 21:42:01 +1100

    update the counter

--- abc_kata/kata_abc.py
+++ abc_kata/kata_abc.py
@@ -11,16 +11,24 @@ blocks =['BO','(X K)','(D Q)','(C P)','(N A)','(G T)','(R E)','(T G)','(Q D)','(
 new_list=[]
 
 def can_make_word(word):
+    count=0
+    index =0
+    while index < len(word):
+        w = word[index]
+        for w in word:
+            for block in blocks:
+                if w in block and block not in new_list:
+                    #create a counter of the word that I do not use same letter again
+                    new_list.append(block)
+                    count+=1
+                    index+=1
+                    print(f'letter {w},{block} is in the list ')
 
-    for w in word:
-        for block in blocks:
-            if w in block and block not in new_list:
-                #create a counter of the word that I do not use same letter again
-                new_list.append(block)
-                print(f'letter {w},{block} is in the list ')
+                else:
+                    print(f"this {block} is taken")
+        break
 
-            else:
-                print(f"this {block} is taken")
+
 # Currently it's checking every time same letter multiple times in the blocks
 
 def user_input():
