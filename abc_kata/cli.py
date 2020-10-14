e6b6f77f9cb9157ba71e82c001f0ca3aaac1d221
Irina Babicheva <irina.babicheva@myob.com>
2020-10-14 20:58:54 +1100

    adding some logic to check blocks and letters (no counter on the letters yet)

---
+++ abc_kata/cli.py
@@ -0,0 +1,6 @@
+from kata_abc import user_input
+from kata_abc import can_make_word
+
+word=user_input()
+print(f"your word : {word}")
+can_make_word(word)
---
+++ abc_kata/kata_abc.py
@@ -0,0 +1,29 @@
+
+# 1. get collection of the blocks
+# 2. be able to prinnt each row (item) from collection
+
+
+blocks =['BO','(X K)','(D Q)','(C P)','(N A)','(G T)','(R E)','(T G)','(Q D)','(F S)','(J W)','(H U)','(V I)',
+         '(A N)','(O B)','(E R)','(F S)','(L Y)','(P C)','(Z M)']
+
+
+
+new_list=[]
+
+def can_make_word(word):
+
+    for w in word:
+        for block in blocks:
+            if w in block and block not in new_list:
+                #create a counter of the word that I do not use same letter again
+                new_list.append(block)
+                print(f'letter {w},{block} is in the list ')
+
+            else:
+                print(f"this {block} is taken")
+# Currently it's checking every time same letter multiple times in the blocks
+
+def user_input():
+    word = input("What is your word?: ")
+    return word.upper()
+
