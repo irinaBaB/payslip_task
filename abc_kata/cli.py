02bd80c6aeb75c2d336949483dc7e7ecb25fa886
Irina Babicheva <irina.babicheva@myob.com>
2020-10-18 16:21:45 +1100

    adding updated code to handle entering letter once

--- abc_kata/cli.py
+++ abc_kata/cli.py
@@ -2,5 +2,5 @@ from kata_abc import user_input
 from kata_abc import can_make_word
 
 word=user_input()
-print(f"your word : {word}")
+# print(f"your word : {word}")
 can_make_word(word)
--- abc_kata/kata_abc.py
+++ abc_kata/kata_abc.py
@@ -3,30 +3,24 @@
 # 2. be able to prinnt each row (item) from collection
 
 
-blocks =['BO','(X K)','(D Q)','(C P)','(N A)','(G T)','(R E)','(T G)','(Q D)','(F S)','(J W)','(H U)','(V I)',
-         '(A N)','(O B)','(E R)','(F S)','(L Y)','(P C)','(Z M)']
+blocks =['BO','XK','DQ','CP','NA','GT','RE','TG','QD','FS','JW','HU','VI','AN','OB','ER','FS','LY','PC','ZM']
 
 
 
 new_list=[]
 
+new_list=[]
 def can_make_word(word):
-    count=0
-    index =0
-    while index < len(word):
-        w = word[index]
-        for w in word:
+        for w in word.upper():
             for block in blocks:
                 if w in block and block not in new_list:
                     #create a counter of the word that I do not use same letter again
                     new_list.append(block)
-                    count+=1
-                    index+=1
                     print(f'letter {w},{block} is in the list ')
+                    break
 
                 else:
                     print(f"this {block} is taken")
-        break
 
 
 # Currently it's checking every time same letter multiple times in the blocks
