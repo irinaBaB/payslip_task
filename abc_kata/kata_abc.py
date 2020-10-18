8a6dd6b58c50cae6f0ec507f5157fe4c16570195
Irina Babicheva <irina.babicheva@myob.com>
2020-10-18 16:56:03 +1100

    adding word check

--- abc_kata/kata_abc.py
+++ abc_kata/kata_abc.py
@@ -6,21 +6,25 @@
 blocks =['BO','XK','DQ','CP','NA','GT','RE','TG','QD','FS','JW','HU','VI','AN','OB','ER','FS','LY','PC','ZM']
 
 
-
 new_list=[]
-
-new_list=[]
+created_word=[]
 def can_make_word(word):
         for w in word.upper():
             for block in blocks:
                 if w in block and block not in new_list:
                     #create a counter of the word that I do not use same letter again
                     new_list.append(block)
+                    created_word.append(w)
                     print(f'letter {w},{block} is in the list ')
                     break
 
                 else:
-                    print(f"this {block} is taken")
+                    print('FALSE')
+
+
+        if created_word == word:
+              print(created_word)
+              print('TRUE')
 
 
 # Currently it's checking every time same letter multiple times in the blocks
