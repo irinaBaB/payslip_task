cdeb2ca45101f19b95e1488046f57acfca30621d
Irina Babicheva <irina.babicheva@myob.com>
2020-10-28 14:56:08 +1100

    test

--- black_jack/game.py
+++ black_jack/game.py
@@ -5,9 +5,6 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,'Queen':10, 'King':10, 'Ace':11}
 
 # 1. Setting up a Card class:
-
-# 1. Setting up a Card class:
-
 class Card():
 
     def __init__(self,suit,rank):
@@ -17,6 +14,8 @@ class Card():
 
     def __str__(self):
         return self.rank + ' of ' + self.suit
+
+    card = Card()
 
 
 
