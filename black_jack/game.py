dfdea3e764f6751998dde95b1823dda243dea6b4
Irina Babicheva <irina.babicheva@myob.com>
2020-10-28 14:23:15 +1100

    update to value of the card

--- black_jack/game.py
+++ black_jack/game.py
@@ -2,7 +2,9 @@ from random import shuffle
 
 suits=('Hearts','Diamonds','Spades','Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,'Queen':10, 'King':10, 'Ace':11}
 
+# 1. Setting up a Card class:
 
 # 1. Setting up a Card class:
 
@@ -11,8 +13,11 @@ class Card():
     def __init__(self,suit,rank):
         self.suit = suit
         self.rank = rank
+        self.value = values[rank]
 
     def __str__(self):
         return self.rank + ' of ' + self.suit
+
+
 
 
