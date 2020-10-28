4e1943f5e6b1dd6c421be19d0630fd424a0c42d5
Irina Babicheva <irina.babicheva@myob.com>
2020-10-29 00:56:05 +1100

    Added new class Player - but not quite working - in progress

--- black_jack/cli.py
+++ black_jack/cli.py
@@ -1,15 +1,26 @@
 
-from game import Card
-from game import values
-from game import Deck
+from cards import Card
+from cards import values
+from cards import Deck
+from players import Player
+
 
 input_value = input ("enter the value: ")
 deck = Deck()
+player_hands = Player()
+dealer_hands = Player()
 deck.shuffle()
 single_card = deck.deal_card()
-
+player_hands.add_card(single_card)
+player_hands.add_card(single_card)
+dealer_hands.add_card(single_card)
+dealer_hands.add_card(single_card)
 while input_value =='1':
-    print(single_card)
+#these won't print me anything as 'Player' not iterable - need to fix it
+    for card in player_hands:
+        print(card)
+    for card in dealer_hands:
+        print(card)
     input_value = input ("enter the value: ")
 
 if input_value == '2':
---
+++ black_jack/players.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self):
+        self.player_hands = []
+
+    def add_card(self,card):
+        self.player_hands.append(card)
+
+
+#player1
+#player2 diller
+# need to add card to player
+
