26936a0ddb72d95e9766dc93ceb7bd432d39fe50
Irina Babicheva <irina.babicheva@myob.com>
2020-11-03 22:02:35 +1100

    adjusting for ace

--- black_jack/cli.py
+++ black_jack/cli.py
@@ -19,9 +19,14 @@ player.add_card(deck.deal_card())
 #dealer taking 2 cards
 dealer.add_card(deck.deal_card())
 dealer.add_card(deck.deal_card())
+
+#adjust for ace
+player.ace_adjust()
+dealer.ace_adjust()
+
 while input_value =='1':
-    print(f"Player has got: \n{player} \n\t With score: { player.value}")
-    print((f"Dealer has got: \n{dealer} \n\t With score: { dealer.value}"))
+    print(f"Player has got: \n{player} \n\t you are currently at : { player.value}")
+    print((f"Dealer has got: \n{dealer} \n\t you are currently at: { dealer.value}"))
     input_value = input ("enter the value: ")
 
 if input_value == '2':
--- black_jack/players.py
+++ black_jack/players.py
@@ -5,15 +5,20 @@ class Player:
     def __init__(self):
         self.player_hands = []
         self.value = 0
+        self.aces = 0
 
 
     def add_card(self,card):
 
         self.player_hands.append(card)
         self.value+=values[card.rank]
+        if card.rank=='Ace':
+            self.aces+=1
 
-    #def ace_adjust(self,card):
-
+    def ace_adjust(self):
+        if self.value >=21 and self.aces>0:
+            self.aces -=1
+            self.value -=10
 
     def __str__(self):
         player_cards = ""
